1bd9740a97210ffddef1a022ba556ac338abf6ae
pug <pug.gg@kakaocorp.com>
2023-03-14 00:10:06 +0900

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    clients: []
+  };
 
+  async componentDidMount() {
+    const response = await fetch('/test/ping');
+    const body = await response.text();
+    this.setState({ping: body});
+  }
+
+  render() {
+    const {ping} = this.state;
+    console.log(ping);
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>ping</h2>
+              {
+                <div>
+                {ping}
+                </div>
+              }
+            </div>
+          </header>
+        </div>
+    );
+  }
+}
 export default App;
