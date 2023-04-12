53e3e213e3c5a5ce9b4c9f97a5e4379ecaf149de
정현 <hyunseo0772@gmail.com>
2023-04-12 16:28:47 +0900

    로그인, 구글 인증 임시 테스트

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,76 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import {Link, useNavigate} from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import './Main.css'
-import { HiOutlineClipboardList } from 'react-icons/hi';
-import {getDailyHealthMessage} from "../api/getDailyHealthMessage";
-import {getUserInfo} from "../api/getUserInfo";
+import React from 'react';
 
 function Main() {
-    let navigate = useNavigate();
-
-    const survey = () => {
-        navigate("/survey");
-    }
-
-
-    const [info, setInfo] = useState({
-        nickname: '',
-        fullName: '',
-    });
-    const [dailyHealthMessage, setDailyHealthMessage] = useState('');
-
-    useEffect(() => {
-        const initUserinfo = async () => {
-            const newinfo = await getUserInfo();
-            setInfo(newinfo);
-            console.log("###" + newinfo.fullName);
-        };
-
-
-        const initDailyHealthMessage = async () => {
-            try {
-                const newMessage = await getDailyHealthMessage()
-                setDailyHealthMessage(newMessage.content)
-            } catch (e) {
-                console.error(e.message)
-                setDailyHealthMessage('설문을 완료하고, 맞춤형 건강 메시지를 받아보세요!')
-            }
-
-        }
-        initDailyHealthMessage()
-        initUserinfo()
-    }, []);
-
     return (
         <div className="main-bg">
             <div className="main">
-                <p>안녕하세요, {info.nickname === null ? info.fullName : info.nickname}님! {dailyHealthMessage}</p>
-
-                <Container>
-                    <Row>
-                        <Col>
-                            <div className="main-col-box" onClick={survey}>
-                                <HiOutlineClipboardList size="200"></HiOutlineClipboardList>
-                                <p>문진하기</p>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div className="main-col-box">
-                                <HiOutlineClipboardList size="200"></HiOutlineClipboardList>
-                                <p>~~</p>
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>1 of 3</Col>
-                        <Col>2 of 3</Col>
-                        <Col>3 of 3</Col>
-                    </Row>
-                </Container>
-
+                본문
             </div>
         </div>
     );
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,180 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import {signUpApi} from "../api/signUpApi"
-import {getUserInfo} from "../api/getUserInfo";
-import { useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
 
-//회원가입
-function SignUp({isLogin}) {
-    //유저 정보 조회
-    const navi = useNavigate();
-    const [info, setInfo] = useState({
-        nickname: "",
-        gender: "",
-        dateOfBirth: "",
-        height: 0.0,
-        weight: 0.0,
-        mbti: "",
-        isSmoking: null,
-        isAlcohol: null,
-        HealthcareType: null,
-        profileImageUrl: '',
-        email: '',
-        firstName: '',
-        lastName: '',
-    });
-    useEffect(() => {
-        if (!isLogin) navi('/login');
-
-        const initUserinfo = async () => {
-            let newinfo = await getUserInfo();
-            setInfo(newinfo);
-        };
-        initUserinfo();
-    }, [isLogin]);
-
-
-    //변수
-    const YEAR = [];
-    const nowYear = new Date().getFullYear();
-    for (let i = 1980; i <= nowYear; i++) {
-        YEAR.push(i);
-    }
-    const MONTH = [];
-    for (let i = 1; i <= 12; i++) {
-        let m = String(i).padStart(2, '0');
-        MONTH.push(m);
-    }
-    const DAY = [];
-    for (let i = 1; i <= 31; i++) {
-        let d = String(i).padStart(2, '0');
-        DAY.push(d);
-    }
-
-    const dateToString = (year, month, day) => {
-        return year + "-" + month + "-" + day
-    }
-    let [formData, setFormData] = useState({
-        nickname: "",
-        gender: "MALE",
-        dateOfBirth: "",
-        height: 0.0,
-        weight: 0.0,
-        mbti: "",
-        isSmoking: null,
-        isAlcohol: null,
-        HealthcareType: null,
-        stressPoint: 0,
-        year: '1980',
-        month: '01',
-        day: '01',
-    });
-
-    const handleInputChange = (event) => {
-        setFormData((prevProps) => ({
-            ...prevProps,
-            [event.target.name]: event.target.value
-        }));
-    };
-    const onSubmit = (data) => {
-        data.preventDefault();
-        const submitData = {
-            nickname: formData.nickname,
-            gender: formData.gender,
-            dateOfBirth: dateToString(formData.year, formData.month, formData.day),
-            height: formData.height,
-            weight: formData.weight,
-            mbti: formData.mbti,
-            isSmoking: formData.isSmoking,
-            isAlcohol: formData.isAlcohol,
-            HealthcareType: formData.HealthcareType,
-            stressPoint: formData.stressPoint,
-        }
-        signUpApi(submitData);
-        navi("/mypage");
-    };
+function SignUp() {
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+    let [confirm, setConfirm] = useState("");
+    let [firstName, setFirstName] = useState("");
+    let [lastName, setLastName] = useState("");
+    let [gender, setGender] = useState("");
+    let [dateOfBirth, setDateOfBirth] = useState("");
+    let [height, setHeight] = useState();
+    let [weight, setWeight] = useState();
+    let [mbti, setMbti] = useState("");
+    /*프로필 이미지 url??*/
 
     return (
         <div className="main-bg">
-            <div className="main">
-                <h3 className="small-title">회원가입</h3>
-                <div className="form-box">
-                    <form onSubmit={onSubmit}>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="email">이메일</label>
-                            <input className="form-string-input" type="email" id="email" name="email" value={info.email} readOnly disabled/>
-                        </div>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="lastname">성</label>
-                            <input className="form-string-input" type="text" id="lastname" name="lastname" value={info.lastName} readOnly disabled />
-                        </div>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="firstname">이름</label>
-                            <input className="form-string-input" type="text" id="firstname" name="firstname" value={info.firstName} readOnly disabled />
-                        </div>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="firstname">별명</label>
-                            <input onChange={handleInputChange} className="form-string-input" type="text" id="nickname" name="nickname" value={formData.nickname}/>
-                        </div>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="gender">성별</label>
-                            <div className="mb-3">
-                                <label>
-                                    <input
-                                        type="radio"
-                                        name="gender"
-                                        value="MALE"
-                                        checked={formData.gender === 'MALE'}
-                                        onChange={handleInputChange}
-                                    />
-                                    남성
-                                </label>
-                                <label>
-                                    <input
-                                        type="radio"
-                                        name="gender"
-                                        value="FEMALE"
-                                        checked={formData.gender === 'FEMALE'}
-                                        onChange={handleInputChange}
-                                    />
-                                    여성
-                                </label>
-                            </div>
-                        </div>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="height">생년월일</label>
-                            <div style={{display: 'flex'}}>
-                                <select className="select" name="year" onChange={handleInputChange}>
-                                    {YEAR.map(y => {
-                                        return <option key={y}>{y}</option>;
-                                    })}
-                                </select>
-                                <select className="select" name="month" onChange={handleInputChange}>
-                                    {MONTH.map(m => {
-                                        return <option key={m}>{m}</option>;
-                                    })}
-                                </select>
-                                <select className="select" name="day" onChange={handleInputChange}>
-                                    {DAY.map(d => {
-                                        return <option key={d}>{d}</option>;
-                                    })}
-                                </select>
-                            </div>
-                        </div>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="height">키</label>
-                            <input onChange={handleInputChange} className="form-string-input" type="text" id="height" name="height" value={formData.height} />
-                        </div>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="weight">몸무게</label>
-                            <input onChange={handleInputChange} className="form-string-input" type="text" id="weight" name="weight" value={formData.weight} />
-                        </div>
-                        <div className="form-input-container">
-                            <label className="form-input-label" htmlFor="mbti">MBTI</label>
-                            <input onChange={handleInputChange} className="form-string-input" type="text" id="mbti" name="mbti" value={formData.mbti} />
-                        </div>
-                        <button type="submit" className="submit-btn">회원가입</button>
-                    </form>
-                </div>
+            <div className="sign-page">
+                <h3 style={{margin: "25px", color: "#E63A35", fontWeight: "bolder", fontSize: "40px", marginBottom: "40px"}}>회원가입</h3>
+                <form>
+                    <label>ㄴㅇㄹ</label>
+                    <input className="signup-input" type="email" name="email"/>
+                    <input className="signup-input" type="password" name="password"/>
+                    <input className="signup-input" type="confirm" name="confirm"/>
+                    <input className="signup-input" type="firstName" name="firstName"/>
+                    <input className="signup-input" type="lastName" name="lastName"/>
+                    <input className="signup-input" type="gender" name="gender"/>
+                    <input className="signup-input" type="dateOfBirth" name="dateOfBirth"/>
+                    <input className="signup-input" type="height" name="height"/>
+                    <input className="signup-input" type="weight" name="weight"/>
+                    <input className="signup-input" type="mbti" name="mbti"/>
+                </form>
             </div>
         </div>
     );
