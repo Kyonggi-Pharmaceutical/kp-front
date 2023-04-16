67d19c0ffb7de42a82a5e236ee27a1079efdb7d1
akman12914 <rnlduadns98@naver.com>
2023-04-17 03:35:12 +0900

    feature : 솔루션 시작, 관리카테고리 선택 페이지 개발

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link, Navigate, Route} from "react-router-dom";
+import StartPage from "./StartPage";
 
 function Main() {
     return (
         <div className="main-bg">
             <div className="main">
                 본문
+                <li><Link to="/survey">설문을 시작하시겠습니까?</Link></li>
             </div>
         </div>
     );
