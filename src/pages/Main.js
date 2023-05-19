43cbbb1bd8f59bd5734c6287f95f509da46ed060
pug <pug.gg@kakaocorp.com>
2023-05-19 23:52:02 +0900

    [OVZC-91] feature: 비로그인 유저 메인 디자인 추가

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -89,12 +89,18 @@ function Main() {
         <div className="main-bg">
             <div className="main">
                 {
-                    info.fullName && (
+                    info.fullName ? (
                         <div className="greeting">
                             <p className="greeting-text">
                                 안녕하세요, {info.nickname ? info.nickname : info.fullName}님!
                             </p>
                             <p className="health-message">{dailyHealthMessage}</p>
+                        </div>
+                    ) : (
+                        <div className="greeting" style={{marginTop: '120px', marginBottom: '120px'}}>
+                            <p className="greeting-text">여러분의 건강을 책임지는 16Healthcare 입니다.</p>
+                            <p className="greeting-text">MBTI에 따른 맞춤형 운동 및 스트레스 관리 방법을 추천받아보세요!</p>
+                            <p className="health-message">우측 상단에 로그인 버튼을 눌러 건강관리를 시작해볼까요?</p>
                         </div>
                     )
                 }
