b4610cb235098eb89931734f65192303898a8d42
akman12914 <rnlduadns98@naver.com>
2023-05-28 22:16:39 +0900

    feature : 스트레스 일일 솔루션 조회

--- src/pages/StressResult.jsx
+++ src/pages/StressResult.jsx
@@ -1,32 +1,24 @@
 import React, {useEffect, useState} from 'react';
 import moment from "moment";
-import {postStressInfo} from '../api/postStressInfo';
 import {saveDailyChecked} from "../api/postDailyChecked";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { useNavigate } from 'react-router-dom';
 import { useLocation } from "react-router-dom";
+import { getUserActivitySolutions } from "../api/activity/getUserActivitySolutions";
 import stress from '../pages/StressSurvey';
 import MBTI from "../api/Mbti/../Mbti";
 import Activity from "../api/Activity/../Activity";
 import styles from './StressSurvey.css';
 import Button from 'react-bootstrap/Button';
 console.log(MBTI);
-console.log(Activity);
 
 
 export default function StressResult({info, setUserInfo, healthGoal, setHealthGoal}) {
     const [activityList, setActivityList] = useState([]);
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const [trueDates, setTrueDates] = useState([]);
 
 
-    const submitData = {
-        userId: '1111',
-        type:'현상유지',
-        activity: [],
-        startStressValue:info.stressPoint
-    };
+
     const navigate = useNavigate();
     const DailyGoal = () => {
         navigate("/dailyGoal");
@@ -36,9 +28,20 @@ export default function StressResult({info, setUserInfo, healthGoal, setHealthGo
     const location = useLocation();
     const value = location.state.value;
 
-    useEffect( ()=>{
-        setHealthGoal(submitData);
-        setUserInfo({stressPoint : value});
+    useEffect( async () => {
+
+
+        const fetchUserActivitySolutions = async () => {
+            try {
+                const activitySolutions = await getUserActivitySolutions();
+                setActivityList(activitySolutions);
+            } catch (error) {
+                console.error('Failed to fetchUserActivitySolutions:', error);
+            }
+        };
+
+        await fetchUserActivitySolutions();
+
     }, []);
 
     return (
@@ -46,7 +49,8 @@ export default function StressResult({info, setUserInfo, healthGoal, setHealthGo
         <div className="stressResult-bg">
             <div className="stressResult">
                 <div className="smallTitle">설문 결과</div>
-                <p>ENFP {`${info.lastName} ${info.firstName}`}님의 스트레스 지수는 {value}점입니다.</p>
+
+                <p>MBTI {` ${info.lastName} ${info.firstName}`}님의 스트레스 지수는 {value}점입니다.</p>
                 {MBTI.map((item) => {
                     return (
                         <div
@@ -56,12 +60,19 @@ export default function StressResult({info, setUserInfo, healthGoal, setHealthGo
                         </div>
                     )
                 })}
+
+                {
+                    Array.isArray(activityList) && activityList.map((activity, idx) => {
+                        return (
+                            <div className="activity-item" key={idx}>
+                                <p>증상: {activity.symptom}</p>
+                                <p>추천활동: {activity.name}</p>
+                            </div>
+                        )
+                    })}
                 <p>{value}점의 스트레스는 현상유지가 필요합니다.</p>
-                <p>ENFP유형은 사회적지지 추구, 정서적 대처, 소망적 사고에 해당하는 스트레스 대처방식을 자주 효과적으로 사용합니다.</p>
-                {Activity.map((item) => { return (<p>{`${info.lastName} ${info.firstName}`}님이 무기력할 때, 적절한 스트레스 관리방식은 {item.type}입니다. 또는 스스로 오늘 할 챌린지 한 가지 이상 정하고 지키기나
-                    인생의 현실적인 목표 설정하기, 자신과 대화하며 현재 기분을 적어보세요.</p>)
-                    submitData.activity = activityList.type;
-                })}
+                <p>유형은 사회적지지 추구, 정서적 대처, 소망적 사고에 해당하는 스트레스 대처방식을 자주 효과적으로 사용합니다.</p>
+
                 <div style={{textAlign: "center"}}>
                     <Button className="daily-button" onClick={DailyGoal}>일일 목표 확인</Button>
                 </div>
