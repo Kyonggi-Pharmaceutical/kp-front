563b604237e2335f81794a402737878e5d87a3c1
pug <pug.gg@kakaocorp.com>
2023-05-30 21:13:12 +0900

    fix: 스트레스 월간만족도

--- src/pages/SatisfactionResult.jsx
+++ src/pages/SatisfactionResult.jsx
@@ -1,18 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useLocation } from "react-router-dom";
-import stress from '../pages/StressSurvey';
+import {useNavigate} from 'react-router-dom';
+import {useLocation} from "react-router-dom";
 import MBTI from "../api/Mbti/../Mbti";
 import Activity from "../api/Activity/../Activity";
-import styles from "./StressSurvey.css";
-import DailyGoal from "./DailyGoal";
-import StressResult from "./StressResult";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import Main from "./Main";
 import {getUserActivitySolutions} from "../api/activity/getUserActivitySolutions";
-import {getUserInfo} from "../api/user/getUserInfo";
 import {renewSolutions} from "../api/stresses/renewSolutions";
+
 console.log(MBTI);
 console.log(Activity);
 
@@ -20,20 +15,32 @@ export default function SatisfactionResult({info, setUserInfo, healthGoal, setHe
     const [show, setShow] = useState(false);
     const [activityList, setActivityList] = useState([]);
 
-    const handleClose = () => setShow(false);
+    const navigate = useNavigate();
+
+    const handleClose = () => {
+        setShow(false);
+        satisfactionSurvey();
+    }
     const handleShow = () => setShow(true);
 
-    const navigate = useNavigate();
     const Main = () => {
         window.location.replace("/main");
     }
+
+    const satisfactionSurvey = () => {
+        window.location.replace("/satisfactionSurvey");
+    }
+    const survey = () => {
+        navigate('/survey');
+    }
+
     const location = useLocation();
     const value = location.state.value;
     let comparedStressPoint = info.stressPoint - value;
     const [visible, setVisible] = useState(false);
 
 
-    useEffect( async () => {
+    useEffect(() => {
         console.log(info.mbti);
         console.log(info.stressPoint);
         console.log(value);
@@ -48,15 +55,15 @@ export default function SatisfactionResult({info, setUserInfo, healthGoal, setHe
             }
         };
 
-        await fetchUserActivitySolutions();
+        fetchUserActivitySolutions();
 
 
-    }, []);
+    }, [comparedStressPoint, info.mbti, info.stressPoint, value]);
 
-    function NO(){
+    function NO() {
         renewSolutions();
 
-        return(
+        return (
             <div className='new-solution'>
                 <div className='new-solution-text'>NEW!!</div>
 
@@ -81,19 +88,28 @@ export default function SatisfactionResult({info, setUserInfo, healthGoal, setHe
 
         <div className="main-bg">
             <div className="main">
-                <h3 style={{margin: "25px", color: "#ed6174", fontWeight: "bolder", fontSize: "40px", marginBottom: "40px"}}>설문 결과</h3>
+                <h3 style={{
+                    margin: "25px",
+                    color: "#ed6174",
+                    fontWeight: "bolder",
+                    fontSize: "40px",
+                    marginBottom: "40px"
+                }}>설문 결과</h3>
                 <p>{`${info.mbti} ${info.lastName} ${info.firstName}`}님의 스트레스 지수는 {comparedStressPoint}점 변화했습니다.</p>
-                <p>해당 건강솔루션이 도움이 되었나요? </p>
+                <p>해당 스트레스 관리 솔루션이 도움이 되어 계속 솔루션을 제공받고 싶으시다면 yes를 눌러주세요!</p>
                 <div style={{textAlign: "center"}}>
-                    <Button variant="danger" onClick={Main} size='lg'style={{backgroundColor: "#ed6174", textAlign: "center", margin: "20px 20px"}}>YES</Button>
-                    <Button variant="danger" onClick={handleShow} size='lg' style={{backgroundColor: "#ed6174", textAlign: "center", margin: "20px 20px"}}>NO</Button>
+                    <Button variant="danger" onClick={handleShow} size='lg'
+                            style={{backgroundColor: "#ed6174", textAlign: "center", margin: "20px 20px"}}>YES</Button>
+                    <Button variant="danger" onClick={survey} size='lg'
+                            style={{backgroundColor: "#ed6174", textAlign: "center", margin: "20px 20px"}}>NO</Button>
                 </div>
 
                 <Modal show={show} onHide={handleClose} animation={false} size="lg" centered>
                     <Modal.Header closeButton>
-                        <Modal.Title style={{color: "#E63A35", marginLeft: "10px"}}><strong>새로운 솔루션이 도착했어요!</strong></Modal.Title>
+                        <Modal.Title style={{color: "#E63A35", marginLeft: "10px"}}><strong>새로운 스트레스 관리를
+                            시작해보세요!</strong></Modal.Title>
                     </Modal.Header>
-                    <Modal.Body style={{fontSize: "20px"}}><NO /></Modal.Body>
+                    <Modal.Body style={{fontSize: "20px"}}><NO/></Modal.Body>
                     <Modal.Footer>
                         <Button variant="danger" onClick={handleClose}>
                             닫기
